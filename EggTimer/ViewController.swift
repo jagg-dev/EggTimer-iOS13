374036e8c3c14873ccf83c1dc6a72ac336e822fc
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-03-23 14:03:03 -0600

    Timer challenge completed.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var counter: Int? = nil
+    var timer: Timer? = nil
     
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        print(eggTimes[hardness!]!)
+        
+        counter = eggTimes[hardness!]
+        
+        if timer != nil {
+            timer?.invalidate()
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateCounter() {
+        if counter! > 0 {
+            print("\(counter!) seconds.")
+            counter! -= 1
+            
+        } else {
+            timer?.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
 
 }
